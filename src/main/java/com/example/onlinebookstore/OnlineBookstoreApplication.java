0f9de5295b3dfc9130283b6b0fbea619fd4bf14f
Oleksandr Hatsenko <a.s.gatsenko@gmail.com>
2023-09-05 18:09:36 +0300

    added dto, mapstruct and mapper

--- src/main/java/com/example/onlinebookstore/OnlineBookstoreApplication.java
+++ src/main/java/com/example/onlinebookstore/OnlineBookstoreApplication.java
@@ -1,35 +1,12 @@
 package com.example.onlinebookstore;
 
-import com.example.onlinebookstore.model.Book;
-import com.example.onlinebookstore.service.BookService;
-import java.math.BigDecimal;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class OnlineBookstoreApplication {
-    @Autowired
-    private BookService bookService;
 
     public static void main(String[] args) {
         SpringApplication.run(OnlineBookstoreApplication.class, args);
     }
-
-    @Bean
-    public CommandLineRunner commandLineRunner() {
-        return args -> {
-            Book threeComrades = new Book();
-            threeComrades.setTitle("Three Comrades");
-            threeComrades.setAuthor("Erich Maria Remarque");
-            threeComrades.setIsbn("9780449912423");
-            threeComrades.setPrice(BigDecimal.valueOf(19.99));
-
-            bookService.save(threeComrades);
-            System.out.println(bookService.findAll());
-        };
-    }
-
 }
--- src/main/java/com/example/onlinebookstore/repository/impl/BookRepositoryImpl.java
+++ src/main/java/com/example/onlinebookstore/repository/impl/BookRepositoryImpl.java
@@ -1,5 +1,6 @@
 package com.example.onlinebookstore.repository.impl;
 
+import com.example.onlinebookstore.exception.EntityNotFoundException;
 import com.example.onlinebookstore.model.Book;
 import com.example.onlinebookstore.repository.BookRepository;
 import java.util.List;
@@ -39,7 +40,16 @@ public class BookRepositoryImpl implements BookRepository {
         try (Session session = sessionFactory.openSession()) {
             return session.createQuery("FROM Book ", Book.class).getResultList();
         } catch (Exception e) {
-            throw new RuntimeException("Can`t get all books", e);
+            throw new EntityNotFoundException("Can`t get all books", e);
+        }
+    }
+
+    @Override
+    public Book getById(Long id) {
+        try (Session session = sessionFactory.openSession();) {
+            return session.get(Book.class,id);
+        } catch (Exception e) {
+            throw new EntityNotFoundException("Can't get book by id: " + id, e);
         }
     }
 }
