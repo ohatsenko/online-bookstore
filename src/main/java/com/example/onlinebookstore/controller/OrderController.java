52c880f21bf9681616d8196ccb16c18bd5598fa7
Oleksandr Hatsenko <a.s.gatsenko@gmail.com>
2023-09-26 23:45:16 +0300

    Implemented order and orderitem

---
+++ src/main/java/com/example/onlinebookstore/controller/OrderController.java
@@ -0,0 +1,74 @@
+package com.example.onlinebookstore.controller;
+
+import com.example.onlinebookstore.dto.CreateOrderRequestDto;
+import com.example.onlinebookstore.dto.OrderDto;
+import com.example.onlinebookstore.dto.OrderItemDto;
+import com.example.onlinebookstore.dto.UpdateStatusOrderDto;
+import com.example.onlinebookstore.service.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import java.util.Set;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Order management", description = "Endpoints for managing orders")
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value = "/orders")
+public class OrderController {
+    private final OrderService orderService;
+
+    @GetMapping
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @Operation(summary = "Get an order history", description
+            = "Get an order history")
+    public Set<OrderDto> getOrder() {
+        return orderService.getOrders();
+    }
+
+    @PostMapping
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    @Operation(summary = "Create a new order", description
+            = "Create a new order")
+    public void createOrder(@RequestBody @Valid CreateOrderRequestDto orderRequestDto) {
+        orderService.createOrder(orderRequestDto);
+    }
+
+    @GetMapping("{orderId}/items")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @Operation(summary = "Get an order items", description
+            = "Get an order items")
+    public Set<OrderItemDto> getOrderItemsByOrderId(@PathVariable Long orderId) {
+        return orderService.getOrderItemsByOrderId(orderId);
+    }
+
+    @GetMapping("{orderId}/items/{itemId}")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @Operation(summary = "Get an order's item", description
+            = "Get an order's item")
+    public OrderItemDto getOrderSpecificItem(@PathVariable Long orderId, @PathVariable
+            Long itemId) {
+        return orderService.getOrderItemByOrderIdAndOrderItemId(orderId, itemId);
+    }
+
+    @PatchMapping("{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    @Operation(summary = "Update order status", description
+            = "Update order status. Admin only")
+    public void updateOrderStatus(@PathVariable Long id,
+                                  @RequestBody UpdateStatusOrderDto orderStatusRequestDto) {
+        orderService.updateOrderStatus(id, orderStatusRequestDto);
+    }
+}
---
+++ src/main/java/com/example/onlinebookstore/model/Order.java
@@ -0,0 +1,54 @@
+package com.example.onlinebookstore.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Set;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+@Entity
+@Data
+@SQLDelete(sql = "UPDATE orders SET is_deleted = true WHERE id=?")
+@Where(clause = "is_deleted=false")
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
+    @Column(nullable = false)
+    private BigDecimal total;
+    @Column(name = "order_date", nullable = false)
+    private LocalDateTime orderDate;
+    @Column(name = "shipping_address", nullable = false)
+    private String shippingAddress;
+    @OneToMany(mappedBy = "order")
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private Set<OrderItem> orderItems;
+    @Column(nullable = false)
+    private boolean isDeleted = false;
+
+    public enum Status {
+        COMPLETED, PENDING, DELIVERED, CONFIRMED
+    }
+}
