5e56ea1c194c9fff867c63d01d7373ef9ae18730
Oleksandr Hatsenko <a.s.gatsenko@gmail.com>
2023-09-27 16:19:01 +0300

    Fixed style

--- src/main/java/com/example/onlinebookstore/dto/CreateOrderRequestDto.java
+++ src/main/java/com/example/onlinebookstore/dto/CreateOrderRequestDto.java
@@ -1,6 +1,5 @@
 package com.example.onlinebookstore.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
